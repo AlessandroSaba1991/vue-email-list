949acf6dcf25f89cb29cc253c8144c94af9cb562
AlessandroSaba1991 <sabaalessandro515@gmail.com>
2022-04-29 14:48:19 +0200

    complete with bonus

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,62 @@
 const app = new Vue({
     el: '#app',
-    data:{},
-    methods:{}
+    data:{
+        data:null,
+        emails:[]
+    },
+    methods:{},
+    mounted(){
+
+        axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+        .then(response => {
+            this.emails.push(response.data.response)
+        })
+
+        axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+        .then(response => {
+            this.emails.push(response.data.response)
+        })
+
+        axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+        .then(response => {
+            this.emails.push(response.data.response)
+        })
+
+        axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+        .then(response => {
+            this.emails.push(response.data.response)
+        })
+
+        axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+        .then(response => {
+            this.emails.push(response.data.response)
+        })
+
+        axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+        .then(response => {
+            this.emails.push(response.data.response)
+        })
+
+        axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+        .then(response => {
+            this.emails.push(response.data.response)
+        })
+
+        axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+        .then(response => {
+            this.emails.push(response.data.response)
+        })
+
+        axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+        .then(response => {
+            this.emails.push(response.data.response)
+        })
+
+        axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+        .then(response => {
+            this.emails.push(response.data.response)
+            this.data = response.data
+        })
+    }
+
 })
