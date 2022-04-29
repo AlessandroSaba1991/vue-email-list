541129a6f86882e8c506b7f55befcb7504d60600
AlessandroSaba1991 <sabaalessandro515@gmail.com>
2022-04-29 15:40:52 +0200

    finito

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,69 +1,16 @@
 const app = new Vue({
-    el: '#app',
-    data:{
-        emails:[],
-    },
-    methods:{},
-    mounted(){
-
-        const new_array = []
-            for (let i=0;i<10;i++){
-
-                axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
-                .then(response => {
-                    new_array.push(response.data.response+i)
-                })
-            }
-                this.emails = new_array
-                
-            
-        
-
-        /* axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
-        .then(response => {
-            this.emails.push(response.data.response)
-        })
-
-        axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
-        .then(response => {
-            this.emails.push(response.data.response)
-        })
-
-        axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
-        .then(response => {
-            this.emails.push(response.data.response)
-        })
-
-        axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
-        .then(response => {
-            this.emails.push(response.data.response)
-        })
-
-        axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
-        .then(response => {
-            this.emails.push(response.data.response)
-        })
-
-        axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
-        .then(response => {
-            this.emails.push(response.data.response)
-        })
-
-        axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
-        .then(response => {
-            this.emails.push(response.data.response)
-        })
-
-        axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
-        .then(response => {
-            this.emails.push(response.data.response)
-        })
-
-        axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
-        .then(response => {
-            this.emails.push(response.data.response)
-            this.data = response.data
-        })
-        */
+  el: "#app",
+  data: {
+    emails: [],
+  },
+  methods: {},
+  mounted() {
+    for (let i = 0; i < 10; i++) {
+      axios
+        .get("https://flynn.boolean.careers/exercises/api/random/mail")
+        .then((response) => {
+          this.emails.push(response.data.response);
+        });
     }
-})
+  },
+});
